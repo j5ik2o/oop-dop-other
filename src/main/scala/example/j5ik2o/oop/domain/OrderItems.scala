b3c42f24c9fec18001004e6c82cc3af0a3d4418d
Junichi Kato <j5ik2o@gmail.com>
2023-12-06 17:31:09 +0900

    wip

--- src/main/scala/example/j5ik2o/oop/domain/OrderItems.scala
+++ src/main/scala/example/j5ik2o/oop/domain/OrderItems.scala
@@ -1,7 +1,6 @@
 package example.j5ik2o.oop.domain
 
 import example.j5ik2o.common.domain.OrderItemId
-import example.j5ik2o.oop.domain.{ Item, OrderItem, Price }
 
 import scala.annotation.targetName
 
