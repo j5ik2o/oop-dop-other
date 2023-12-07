ea866955b40237ec77570d1c7d4be8593545da35
Junichi Kato <j5ik2o@gmail.com>
2023-12-07 11:29:53 +0900

    wip

--- src/main/scala/example/j5ik2o/dop/domain/Item.scala
+++ src/main/scala/example/j5ik2o/dop/domain/Item.scala
@@ -1,6 +1,39 @@
 package example.j5ik2o.dop.domain
 
-import example.j5ik2o.common.domain.{ ItemId, ItemName, ItemType }
-import example.j5ik2o.dop.domain.Price
+import example.j5ik2o.common.domain.{ItemId, ItemName, ItemType}
 
-final case class Item(id: ItemId, itemType: ItemType, name: ItemName, price: Price)
+sealed trait Item {
+  def id: ItemId
+
+  def itemType: ItemType
+
+  def name: ItemName
+
+  def price: Price
+}
+
+final case class GenericItem(id: ItemId, name: ItemName, price: Price) extends Item {
+  val itemType: ItemType = ItemType.Generic
+}
+
+final case class DownloadableItem(id: ItemId, name: ItemName, price: Price) extends Item {
+  val itemType: ItemType = ItemType.Download
+}
+
+final case class CarItem(id: ItemId, name: ItemName, price: Price) extends Item {
+  val itemType: ItemType = ItemType.Car
+}
+
+final case class ElectricPartItem(id: ItemId, name: ItemName, price: Price) extends Item {
+  val itemType: ItemType = ItemType.ElectricParts
+}
+
+final case class ElectricAssemblyPartItem(id: ItemId, name: ItemName, parts: Vector[ElectricPartItem]) extends Item {
+  val itemType: ItemType = ItemType.ElectricParts
+
+  override def price: Price = {
+    parts.foldLeft(Price.zero()) { (acc, part) =>
+      Price.plus(acc, part.price)
+    }
+  }
+}
--- src/main/scala/example/j5ik2o/oop/domain/Item.scala
+++ src/main/scala/example/j5ik2o/oop/domain/Item.scala
@@ -1,6 +1,44 @@
 package example.j5ik2o.oop.domain
 
-import example.j5ik2o.common.domain.{ ItemId, ItemName, ItemType }
-import example.j5ik2o.oop.domain.Price
+import example.j5ik2o.common.domain.{ItemId, ItemName, ItemType}
 
-final case class Item(id: ItemId, itemType: ItemType, name: ItemName, price: Price)
+sealed trait Item {
+  def id: ItemId
+
+  def itemType: ItemType
+
+  def name: ItemName
+
+  def price: Price
+}
+
+object Item {
+  final case class GenericItem(id: ItemId, name: ItemName, price: Price) extends Item {
+    override val itemType: ItemType = ItemType.Generic
+  }
+
+  final case class DownloadableItem(id: ItemId, name: ItemName, price: Price) extends Item {
+    override val itemType: ItemType = ItemType.Download
+  }
+
+  final case class CarItem(id: ItemId, name: ItemName, price: Price) extends Item {
+    override val itemType: ItemType = ItemType.Car
+  }
+
+  final case class ElectricPartItem(id: ItemId, name: ItemName, price: Price) extends Item {
+    override val itemType: ItemType = ItemType.ElectricParts
+  }
+
+  final case class ElectricAssemblyItem(id: ItemId, name: ItemName, parts: Vector[ElectricPartItem]) extends Item {
+    override val itemType: ItemType = ItemType.ElectricParts
+
+    override def price: Price = parts.foldLeft(Price.zero()) { (acc, part) =>
+      acc + part.price
+    }
+
+    def addPartItem(partItem: ElectricPartItem): ElectricAssemblyItem = copy(parts = parts :+ partItem)
+
+    def removePartItem(partItemId: ItemId): ElectricAssemblyItem = copy(parts = parts.filterNot(_.id == partItemId))
+  }
+
+}
